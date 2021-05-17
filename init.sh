6b17e103e7f5e93933fb8f68b201a876ff147f44
Risav Karna <karna@risav.dev>
2021-05-17 21:34:11 +0545

    (chore) remove d-angular-nginx if it exists

--- init.sh
+++ init.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 yarn run install:clean
 npx nx build --prod
+
 docker build -t d-angular .
+
+if [[ "$(docker images -q d-angular-nginx:latest 2> /dev/null)" == "" ]]; then
+  docker kill d-angular-nginx
+  docker rm d-angular-nginx
+fi
+
 docker run --name d-angular-nginx -d -p 8080:80 d-angular
